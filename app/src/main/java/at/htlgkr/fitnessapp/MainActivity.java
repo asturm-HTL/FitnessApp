189f2b2b689fb2e6d7733c7088051118dbecc89e
Andreas Sturm <Andi_Sturm@gmx.at>
2020-05-22 23:34:04 +0200

    Prototype-POST-Request, not working; Added Loginscreen Image

--- app/src/main/java/at/htlgkr/fitnessapp/MainActivity.java
+++ app/src/main/java/at/htlgkr/fitnessapp/MainActivity.java
@@ -13,9 +13,13 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.ExecutionException;
 
@@ -34,6 +38,7 @@ public class MainActivity extends AppCompatActivity
 
         View view = new View(this);
         getAllToDos(view);
+        addToDo(view);
     }
 
 
@@ -138,4 +143,113 @@ public class MainActivity extends AppCompatActivity
             e.printStackTrace();
         }
     }
+    //---------------------------GETENDE------------------------
+
+    //-----------------POSTTEST--------------------------------
+    
+    public static String testusername = "testusername";
+    public static String testlastname = "testlastname";
+    public static String testfirstname = "testfirstname";
+    public static String testpassword = "tespassword";
+    public static String userstring;
+
+    public class ToDoAddTask extends AsyncTask<String, Integer, String>
+    {
+
+
+        private static final String linkURL = "http://www.fitnesscenter-mitter.at/userdata.php";
+
+        @Override
+        protected void onPreExecute()
+        {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values)
+        {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Log.d("TAG", "doInBackground: ");
+
+            try
+            {
+                HttpURLConnection connection = (HttpURLConnection) new URL(linkURL + "?firstname="+testfirstname+"&lastname="+testlastname+"&username="+testusername+"&password="+testpassword).openConnection();
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type","application/json");
+                OutputStream os = connection.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
+                writer.write(encodeUserInfos(testfirstname, testlastname, testusername, testpassword));
+                System.out.println("IS_username: " +testusername);
+                System.out.println("IS_password: " +testpassword);
+                System.out.println("IS_firstname: " +testfirstname);
+                System.out.println("IS_lastname: " +testlastname);
+
+                writer.flush();
+                writer.close();
+                os.close();
+
+
+                int responseCode = connection.getResponseCode();
+                Log.d("ResponseMOIS", "ResponseMOIS: " + responseCode);
+                if (responseCode == HttpURLConnection.HTTP_OK)
+                {
+                    BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    StringBuffer sb = new StringBuffer("");
+                    String line = "";
+                    while((line = br.readLine()) != null)
+                    {
+                        sb.append(line);
+                        break;
+                    }
+                    br.close();
+
+                    return sb.toString();
+                }
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
+        private String encodeUserInfos(String... strings)
+        {
+            String s = "{\n " + "\"firstname\": \"" + strings[0] + "\",\n " + "\"lastname\": \""+ strings[1] +"\",\n" + "\"username\" : \""+ strings[2] +"\",\n" + "\"password\" : \""+ strings[3]+"\"\n" + "}";
+            return s;
+
+        }
+    }
+
+    public void addToDo(View view)
+    {
+
+        MainActivity.ToDoAddTask task = new MainActivity.ToDoAddTask();
+        task.execute();
+
+        try
+        {
+            userstring = task.get();
+            System.out.println("MOIS_list_"+userstring);
+
+        }
+        catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    //-------------------------------------
 }
