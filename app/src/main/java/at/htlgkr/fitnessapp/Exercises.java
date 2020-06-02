5cca33c6db863b2beffa2c940088b4329ac22315
Andreas Sturm <Andi_Sturm@gmx.at>
2020-06-02 11:55:49 +0200

    Exercises splits, not done

--- app/src/main/java/at/htlgkr/fitnessapp/Exercises.java
+++ app/src/main/java/at/htlgkr/fitnessapp/Exercises.java
@@ -2,7 +2,23 @@ package at.htlgkr.fitnessapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 public class Exercises extends AppCompatActivity
 {
@@ -15,11 +31,18 @@ public class Exercises extends AppCompatActivity
     public static String password;
     //---------------------------MainVariables-End-----------------------------
 
+    public String exercisesGetRequest;
+
+    public List imglinks = new ArrayList();
+    public List exBeforeSplit = new ArrayList();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercises);
+        View view = new View(this);
 
         //-------DEBUG---------
         System.out.println("MOIS_C_id_"+id);
@@ -29,5 +52,128 @@ public class Exercises extends AppCompatActivity
         System.out.println("MOIS_C_password_"+password);
         //-----DEBUG-End------
 
+
+        getExercises(view);
     }
+
+    //-----------------------------------------GetExercises-----------------------------------------
+
+    private class GetExercises extends AsyncTask<String, Integer, String>
+    {
+
+        private final String TAG = "DieserTag";
+
+        private static final String serverURL = "http://www.fitnesscenter-mitter.at/exercises.php";
+
+        @Override
+        protected String doInBackground(String... strings)
+        {
+            String sJson = "";
+
+            try
+            {
+                HttpURLConnection connection = (HttpURLConnection) new URL(serverURL).openConnection();
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty("Content-Type", "application/json");
+                int responseCode = connection.getResponseCode();
+
+                if (responseCode == HttpURLConnection.HTTP_OK)
+                {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    sJson = readResponseStream(reader);
+                }
+
+            }
+            catch (IOException e)
+            {
+                Log.d(TAG, e.getLocalizedMessage());
+            }
+
+            return sJson;
+        }
+
+        private String readResponseStream(BufferedReader reader) throws IOException
+        {
+            Log.d(TAG, "entered readResponseStrem");
+
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = "";
+
+            while ( (line=reader.readLine()) != null)
+            {
+                stringBuilder.append(line);
+            }
+
+            return stringBuilder.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String s)
+        {
+            Log.d(TAG, "entered onPostExecute");
+            outputExercises(s);
+            super.onPostExecute(s);
+        }
+
+        private void outputExercises(String s)
+        {
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            try
+            {
+                JSONArray array = new JSONArray(s);
+
+                for (int i=0 ; i < array.length() ; i++)
+                {
+                    JSONObject jsonObject = array.getJSONObject(i);
+                    stringBuilder.append(jsonObject);
+                    stringBuilder.append(("||"));
+                }
+            }
+            catch (JSONException e)
+            {
+                Log.d(TAG, e.getLocalizedMessage());
+            }
+        }
+    }
+
+    public void getExercises(View view)
+    {
+
+        Exercises.GetExercises task = new Exercises.GetExercises();
+        task.execute();
+
+        try
+        {
+            exercisesGetRequest  = task.get();
+
+            String[] exparts1 = exercisesGetRequest.split("\\{");
+
+            for(int i = 0; i < exparts1.length; i++)
+            {
+                exBeforeSplit.add(exparts1[i].toString());
+            }
+
+            for(int j = 0; j<exBeforeSplit.size(); j++)
+            {
+                String[] exparts2 = exBeforeSplit.get(j).toString().split(",\"");
+                for(int x = 0; x < exparts2.length; x++)
+                {
+                    System.out.println("EXPARTS_["+x+"]" + exparts2[x].toString());
+                }
+            }
+
+            System.out.println("MOIS_Exercises_: " + exercisesGetRequest);
+        }
+        catch (ExecutionException e)
+        {
+            e.printStackTrace();
+        }
+        catch (InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+    }
+    //----------------------------------------GetExercises-End---------------------------------------
 }
